b115d54a9086c19d051c448829b270ff7832f4a0
SangKwonNo <121703497+SangKwonNo@users.noreply.github.com>
2023-01-30 09:36:45 +0900

    aa

--- NSK_MallProject/src/menu_order/Order_Main.java
+++ NSK_MallProject/src/menu_order/Order_Main.java
@@ -12,20 +12,16 @@ public class Order_Main implements MenuCommand {
 	public void init() {
 		mallCon = MallController.getMallCon();
 		util = Util.getUtil();
-		
+
 	}
 
 	@Override
 	public String update() {
 		String key = "Order_Main";
-		int sel = util.getInt(getMenu(), 0, 4);
+		int sel = util.getInt(getMenu(), 0, 1);
 		if (sel == 1) {
-			key = "Order_DeliveryTrue";
-		} else if (sel == 2) {
-			key = "Order_DeliveryFalse";
-		} else if (sel == 3) {
 			key = "Order_PrintAll";
-		} else if (sel == 4) {
+		} else if (sel == 2) {
 			key = "Order_PrintSearchOrder";
 		} else if (sel == 0) {
 			key = "Admin_Main";
@@ -34,7 +30,7 @@ public class Order_Main implements MenuCommand {
 	}
 
 	private String getMenu() {
-		String menu = "[1.배송완료]\n[2.배송취소]\n[3.전체주문목록]\n[4.주문목록검색]\n[0.뒤로가기]";
+		String menu = "[1.전체주문목록][2.주문목록검색]\n[0.뒤로가기]";
 		return menu;
 	}
 
--- NSK_MallProject/src/order/OrderDAO.java
+++ NSK_MallProject/src/order/OrderDAO.java
@@ -3,7 +3,6 @@ package order;
 import java.util.ArrayList;
 
 import cart.Cart;
-import item.Item;
 
 public class OrderDAO {
 	private static OrderDAO orderDAO = new OrderDAO();
@@ -27,19 +26,6 @@ public class OrderDAO {
 	}
 
 	public void insertData(String idData, String itemData) {
-		String[] temp = data.split(":");
-		Item item = null;
-		for (int i = 0; i < temp.length; i++) {
-			if (i % 2 == 1) {
-				String[] info = temp[i].split("\n");
-				String[] ItemInfo = info[i].split("/");
-				item = new Item(ItemInfo[0], ItemInfo[1], Integer.parseInt(ItemInfo[2]));
-			}
-			for (ArrayList<Cart> cartList : orderList) {
-				cartList.add(new Cart(temp[0], item));
-				System.out.println(cartList.toString());
-			}
-		}
-
+		
 	}
 }
